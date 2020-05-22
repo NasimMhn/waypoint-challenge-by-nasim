ef5d2a2ed9a29a39e162ea6de4fb029f9d8dfa03
nasimmhn <nasim.mahzoun@gmail.com>
2020-05-22 23:16:22 +0200

    started working on solution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
 import waypoints from './waypoints.json'
+import { getPreciseDistance } from 'geolib';
+
+const w1 = waypoints[0]
+const w2 = waypoints[1]
+console.log(waypoints)
+
+// console.log(w1, w2)
+
+const dist = getPreciseDistance(w1.position, w2.position)
+
+const speed = (distance: number, time: number): number => {
+  return distance / time
+}
+
+const timeDifference = []
+const distanceDifference = []
+for (let i = 0; i < waypoints.length - 1; i++) {
+  const time1: any = new Date(waypoints[i].timestamp)
+  const time2: any = new Date(waypoints[i + 1].timestamp)
+  timeDifference.push(time2 - time1)
+  distanceDifference.push(getPreciseDistance(waypoints[i + 1].position, waypoints[i].position))
+
+}
+console.log(timeDifference, distanceDifference)
